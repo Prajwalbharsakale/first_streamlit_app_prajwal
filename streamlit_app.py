bed47fbe13665e13768d04ec7385fa21c75c2dbc
Prajwalbharsakale <89308826+Prajwalbharsakale@users.noreply.github.com>
2023-02-22 11:45:02 +0530

    Update streamlit_app.py
    added another item avocado toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 streamlit.title('My New Helathy Menu')
 
 streamlit.header('Breakfast Menu')
+
 streamlit.text('🍪🥟Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍹🍶Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚🍗Hard-Boiled Free-Range Egg')
-
+streamlit.text('🥑🍞Avocado Toast')
