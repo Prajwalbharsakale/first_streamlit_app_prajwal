eabe4994fd7d854c6a8104a49880d4a80ad2b2dc
Prajwalbharsakale <89308826+Prajwalbharsakale@users.noreply.github.com>
2023-03-14 12:11:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
-import pandas
-import requests
+#import pandas
+#import requests
 import snowflake.connector
-import urllib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('My New Helathy Menu')
 
